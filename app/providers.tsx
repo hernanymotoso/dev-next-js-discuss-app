ae504625d7f3686411ccbb996079704da19804dd
Hernany Motoso <hernanymotoso@gmail.com>
2025-01-04 01:37:50 -0300

    chore: add and config nextui

---
+++ app/providers.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { NextUIProvider } from '@nextui-org/react';
+
+type ProvidersProps = {
+    children: React.ReactNode;
+}
+
+export function Providers({children}: ProvidersProps) {
+    return (
+        <NextUIProvider>
+            {children}
+        </NextUIProvider>
+    )
+}
