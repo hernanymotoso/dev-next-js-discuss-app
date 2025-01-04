3c211d69914402582420ad646c2c9c67fe1e3c15
Hernany Motoso <hernanymotoso@gmail.com>
2025-01-04 20:43:06 -0300

    feat: create topic-create-form componentr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import TopicCreateForm from "@/components/topics/topic-create-form";
 
 export default function Home() {
   return (
-    <div>  
-      <p>Home</p>       
+    <div className="flex justify-between gap-4 p-4">
+      <div>
+        <h1 className="text-xl m-2">Top Posts</h1>  
+      </div>  
+
+      <div>
+        <TopicCreateForm />
+      </div>       
     </div>
   );
 }
